c43e724e93f14f3eae9ed7eec1d96930e208724e
Robert Nugent <d00244618@student.dkit.ie>
2024-10-27 17:59:18 +0000

    Create Navbar component and associated css

---
+++ upflux-client/src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import logo from "../assets/logos/logo-light.png";
+import '../features/Navbar/navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <div className="navbar-logo">
+         <img src={logo} alt="Logo" className="logo" />
+        </div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
