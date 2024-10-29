c30b1c85c770f30ef3137f5fa53f42a7006aae5b
Robert Nugent <d00244618@student.dkit.ie>
2024-10-29 23:14:55 +0000

    Implement Mantine components in Navbar.tsx

--- upflux-client/src/features/Navbar/Navbar.tsx
+++ upflux-client/src/features/Navbar/Navbar.tsx
@@ -1,18 +1,18 @@
+import { Container, Image } from '@mantine/core';
 import logo from "../../assets/logos/logo-light-large.png";
 import './navbar.css';
 
 export const Navbar = () => {
   return (
-    <nav className="navbar">
+    <Container fluid className="navbar">
       <div className="navbar-logo">
-        <img src={logo} alt="Logo" className="logo" />
+        <Image src={logo} alt="Logo" className="logo" />
       </div>
       <ul className="navbar-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-    </nav>
+    </Container>
   );
 };
-
