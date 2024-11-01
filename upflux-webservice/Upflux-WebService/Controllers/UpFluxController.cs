be07df4189a46876987a64557b14a4947c2f6564
Gursimar Singh Ply <49545220+gursimar03@users.noreply.github.com>
2024-11-01 16:27:04 +0000

    54296 - Created initial project & added about controller (#5)
    * created initial project & added about controller
    
    * added ignore for .vs folder
    
    * test
    
    * Remove .vs folder from tracking

---
+++ upflux-webservice/Upflux-WebService/Controllers/UpFluxController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Upflux_WebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UpFluxController : ControllerBase
+    {
+
+        [HttpGet("about", Name = "About")]
+        public string About()
+        {
+            var appName = Assembly.GetExecutingAssembly().GetName().Name;
+            var appVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+
+            return $"Application Name: {appName}, Version: {appVersion}";
+        }
+    }
+}
