e916cf75542a76ad12522b592d8ab2ab7670a356
Robert Nugent <d00244618@student.dkit.ie>
2024-11-17 15:13:59 +0000

    Add Login Route to App

--- upflux-client/src/App.tsx
+++ upflux-client/src/App.tsx
@@ -1,34 +1,39 @@
 import logo from "./assets/logos/logo-dark.png";
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import { Navbar } from './features/Navbar/Navbar'; 
 import { Header } from './features/Header/Header'; 
 import { About } from './features/About/About';
 import { ContactUs } from './features/ContactUs/ContactUs';
 import { Footer } from './features/Footer/Footer';
+import { LoginComponent } from './features/Login/Login'; // Import the LoginComponent
 import { MantineProvider } from "@mantine/core";
 
-
 export const App = () => {
-
   return (
     <MantineProvider>
-    <Router>
-      <section id="home">
-    <Navbar />
-    </section>
-    <Header />
+      <Router> 
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <>
+                <Navbar />
+                <Header />
+                <section id="about">
+                  <About />
+                </section>
+                <section id="contact">
+                  <ContactUs />
+                </section>
+                <Footer />
+              </>
+            } 
+          />
 
-    <section id="about">
-    <About/>
-    </section>
-
-    <section id="contact">
-    <ContactUs/>
-    </section>
-    <Footer/>
-  </Router>
-</MantineProvider>
+          <Route path="/login" element={<LoginComponent />} />
+        </Routes>
+      </Router>
+    </MantineProvider>
   );
-}
-
+};
