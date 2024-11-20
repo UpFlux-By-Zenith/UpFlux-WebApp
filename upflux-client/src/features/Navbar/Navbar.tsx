d1039a42cd811910b2e10cc278eb07e2a79f5a47
Robert Nugent <d00244618@student.dkit.ie>
2024-11-20 14:21:48 +0000

    Improve Header and Navbar CSS

--- upflux-client/src/features/Navbar/Navbar.tsx
+++ upflux-client/src/features/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Container, Image } from '@mantine/core';
 import { Link } from 'react-router-dom'; 
-import logo from "../../assets/logos/logo-light-large.png";
+import logo from "../../assets/logos/logo-no-name.png";
 import './navbar.css';
 
 export const Navbar = () => {
@@ -11,9 +11,9 @@ export const Navbar = () => {
       </div>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>        
-        <li><Link to="/#about">About</Link></li>     
-        <li><Link to="/#contact">Contact</Link></li> 
-        <li><Link to="/login">Login</Link></li>      
+        <li><a href="#about">About</a></li> 
+        <li><a href="#contact">Contact</a></li> 
+        <li><Link to="login">Login</Link></li>      
       </ul>
     </Container>
   );
