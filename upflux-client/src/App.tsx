1b4bfe1b5eccb6dab4c6e5abb9739b6789c90ab0
Robert Nugent <d00244618@student.dkit.ie>
2024-11-23 14:50:13 +0000

    Update Navbar to change after logging in.

--- upflux-client/src/App.tsx
+++ upflux-client/src/App.tsx
@@ -1,25 +1,26 @@
 import logo from "./assets/logos/logo-dark.png";
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import { Navbar } from './features/Navbar/Navbar'; 
-import { Header } from './features/Header/Header'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './features/Navbar/Navbar';
+import { Header } from './features/Header/Header';
 import { About } from './features/About/About';
 import { ContactUs } from './features/ContactUs/ContactUs';
 import { Footer } from './features/Footer/Footer';
-import { LoginComponent } from './features/Login/Login'; 
+import { LoginComponent } from './features/Login/Login';
 import { MantineProvider } from "@mantine/core";
 import { PasswordSettingsContent } from "./features/PasswordSettings/PasswordSettings";
 
 export const App = () => {
   return (
     <MantineProvider>
-      <Router> 
+      <Router>
         <Routes>
-          <Route 
-            path="/" 
+          {/* Home route */}
+          <Route
+            path="/"
             element={
               <>
-                <Navbar />
+                <Navbar home={true} /> {/* Pass home=true */}
                 <Header />
                 <section id="about">
                   <About />
@@ -29,22 +30,23 @@ export const App = () => {
                 </section>
                 <Footer />
               </>
-            } 
+            }
           />
 
+          {/* Login route */}
           <Route path="/login" element={<LoginComponent />} />
 
+          {/* Password settings route */}
           <Route
-          path="/password-settings"
-          element={
-            <>
-            <Navbar />
-              <PasswordSettingsContent />
-              <Footer />
-            </>
-          }
-        />
-
+            path="/password-settings"
+            element={
+              <>
+                <Navbar home={false} /> {/* Pass home=false */}
+                <PasswordSettingsContent />
+                <Footer />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </MantineProvider>
--- upflux-client/src/features/Navbar/Navbar.tsx
+++ upflux-client/src/features/Navbar/Navbar.tsx
@@ -1,19 +1,37 @@
+import React from 'react';
 import { Container, Image } from '@mantine/core';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import notifBell from "../../assets/images/notif_bell.png";
 import logo from "../../assets/logos/logo-no-name.png";
 import './navbar.css';
 
-export const Navbar = () => {
+// Accept a prop `home` to conditionally render the navigation items
+interface NavbarProps {
+  home: boolean;
+}
+
+export const Navbar: React.FC<NavbarProps> = ({ home }) => {
   return (
     <Container fluid className="navbar">
       <div className="navbar-logo">
-        <Image src={logo} alt="Logo" className="logo"/>
+        <Image src={logo} alt="Logo" className="logo" />
       </div>
       <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>        
-        <li><a href="#about">About</a></li> 
-        <li><a href="#contact">Contact</a></li> 
-        <li><Link to="login">Login</Link></li>      
+        {home ? (
+          <>
+            <li><Link to="/">Home</Link></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><Link to="login">Login</Link></li>
+          </>
+        ) : (
+          <>
+            <li className="notification-icon">
+              <Image src={notifBell} alt="Notifications" className="notif-bell" />
+            </li>
+            <li className="profile"><Link to="profile">Profile</Link></li>  
+          </>
+        )}
       </ul>
     </Container>
   );
