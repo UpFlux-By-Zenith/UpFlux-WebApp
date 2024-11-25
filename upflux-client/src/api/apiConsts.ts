e4b9070071c96a57cca9b067e97612e32a011edb
Robert Nugent <d00244618@student.dkit.ie>
2024-11-25 19:11:26 +0000

    Adjust API endpoint constants

--- upflux-client/src/api/apiConsts.ts
+++ upflux-client/src/api/apiConsts.ts
@@ -3,7 +3,7 @@ export const API_BASE_URL = 'https://localhost:7034/api';
 // Auth-related endpoints
 export const AUTH_API = {
   LOGIN: `${API_BASE_URL}/Auth/engineer/login`,
-  CHANGE_PASSWORD: `${API_BASE_URL}/Auth/engineer/change-password`,
+  CHANGE_PASSWORD: `${API_BASE_URL}/Auth/admin/change-password`,
 };
 
 
