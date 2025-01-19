4d397e305ba085048c948d1ac2dbb6803b7274c8
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2025-01-19 14:19:10 +0000

    55472 - Create Cluster Management Page (#30)
    * Create ClusterManagement component
    
    * Add styling and adjust layout
    
    * Adjust spacing and layout
    
    * Update the select options

---
+++ upflux-client/src/features/clusterManagement/ClusterManagement.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Box, Button, Group, Stack, Table, Text, Badge, Select } from "@mantine/core";
+import "./clusterManagement.css";
+import "@mantine/core/styles.css";
+import "@mantine/charts/styles.css";
+import "@mantine/dates/styles.css";
+
+
+export const ClusterManagement: React.FC = () => {
+
+  return (
+    <Stack className="cluster-management-content">
+
+      <Box className="cluster-content-wrapper">
+      {/* Header */}
+      <Box className="header">
+        <Text size="xl" fw={700}>
+          Cluster Management
+        </Text>
+      </Box>
+
+
+      <Stack className="select-group">
+      <Text className="cluster-text" style={{fontWeight: "bold"}}>
+              Cluster 001
+      </Text>
+
+      <Select
+          data={["Update Status", "Number of Devices", "Memory", "Network Status", "Suggested time to update"]}
+          placeholder="Selection"
+          rightSection={null}
+          className="dropdown"
+        />
+
+      </Stack>
+
+        {/* Action Buttons */}
+          <Stack className="cluster-button-group">
+             <Button className="simulate-button">Simulate</Button>
+             <Button className="update-button">Update Now</Button>
+          </Stack>
+       
+      </Box>
+    </Stack>
+  );
+};
