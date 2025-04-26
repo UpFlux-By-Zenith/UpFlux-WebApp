35c7a17e973430792e65f45234550f467b6d526b
Robert Nugent <d00244618@student.dkit.ie>
2025-04-26 12:20:43 +0100

    Update usage of auth token for redux subscription

--- upflux-client/src/features/machineDetails/MachineDetails.tsx
+++ upflux-client/src/features/machineDetails/MachineDetails.tsx
@@ -12,11 +12,12 @@ import {
 import { useMediaQuery } from "@mantine/hooks";
 import "./machineDetails.css";
 import ReactSpeedometer from "react-d3-speedometer";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { RootState } from "../reduxSubscription/store";
 import { getMachineDetails } from "../../api/applicationsRequest";
 import { guestLoginSubmit } from "../../api/loginRequests";
+import { useSubscription } from "../reduxSubscription/useSubscription";
 
 export const MachineDetails: React.FC = () => {
   const storedMachines = useSelector((root: RootState) => root.machines.messages);
@@ -27,12 +28,14 @@ export const MachineDetails: React.FC = () => {
   const [selectedMachineId, setSelectedMachineId] = useState("");
   const [appVersions, setAppVersions] = useState<{ appName: string; appVersion: string; lastUpdate: string }[]>([]);
   const [availableVersions, setAvailableVersions] = useState<string[]>([]);
+  const [authToken, setAuthToken] = useState<string | null>(null);
 
   useEffect(() => {
     const getGuestToken = async () => {
       const token = await guestLoginSubmit();
       if (token) {
         sessionStorage.setItem('authToken', token);
+        setAuthToken(token);
       }
       else{
         console.log("Failed to fetch guest token");
@@ -41,6 +44,8 @@ export const MachineDetails: React.FC = () => {
 
     getGuestToken();
   }, []);
+
+  useSubscription(authToken);
 
   const location = useLocation();
   const isMobile = useMediaQuery("(max-width: 768px)");
